445b14da71f285ae7f3c4dc15381b9fa1001a7fe
NganNguyen0308 <nguyenngannn0308@gmail.com>
2025-02-17 18:21:31 +0700

    Problem2: Updated margin for mobile screen

--- src/problem2/currency-swap/src/components/SwapForm.tsx
+++ src/problem2/currency-swap/src/components/SwapForm.tsx
@@ -94,7 +94,7 @@ const SwapForm = () => {
         <button
           type="button"
           onClick={handleSwapTokens}
-          className="text-lg text-slate-100 cursor-pointer hover:text-slate-300 mt-8"
+          className="text-lg text-slate-100 cursor-pointer hover:text-slate-300 md:mt-6 lg:mt-8"
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
